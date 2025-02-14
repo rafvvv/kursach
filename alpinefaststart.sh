730b411b76f6a034f1d5aac9540076036d649ee1
b1to_me <96476201+rafvvv@users.noreply.github.com>
2025-02-14 20:21:05 +0300

    Update alpinefaststart.sh

--- alpinefaststart.sh
+++ alpinefaststart.sh
@@ -9,7 +9,6 @@ apk add wget
 apk add curl
 apk add neofetch
 apk add gping
-fish
 mkdir server
 cd server
 wget https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar
@@ -17,3 +16,4 @@ apk add openjdk8
 java -Xmx1024M -Xms1024M -jar server.jar nogui
 nano eula.txt
 echo "setup complete"
+fish
